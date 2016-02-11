2e606accc7b9783304198d9eb3924e4a857636b0
JITU <JITU@192.168.2.14>
2016-02-11 15:12:54 +0000

    Complite program

--- newjava/src/newjava/MYProgram.java
+++ newjava/src/newjava/MYProgram.java
@@ -7,12 +7,23 @@ public class MYProgram {
 		Scanner sc =new Scanner(System.in);
 		String i= sc.next();
 		int k= i.length();
-		for(int j=0;j<i.length()/2;j++,k--)
+		k--;
+		for(int j=0; ;j++,k--)
 		{
 			char a=i.charAt(j);
-			char b=i.charAt(k-1);
+			char b=i.charAt(k);
 			if(a==b)
-			{}
+			{
+				if(j>=k)
+				{
+					System.out.println("this is");
+					break;
+				}
+			}
+			else{
+				System.out.println("Not a palendrom");
+				break;
+			}
 		}
 	}
 
